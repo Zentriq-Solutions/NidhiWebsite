2e1cc8a0eaaf2739f388a278375cd57ee01233f0
Zentriq-Solutions <zentriqtechnologies@gmail.com>
2026-07-19 22:37:23 +0530

    Update the code for Item Group

--- NidhiWebsite/Controllers/ItemGroupController.cs
+++ NidhiWebsite/Controllers/ItemGroupController.cs
@@ -54,8 +54,8 @@ namespace NidhiWebsite.Controllers
                     data.item_group_image = fileName;
                 }
                 var saved = SaveItemGroup(data);
-                    return Ok(saved);
-                
+                return Ok(saved);
+
             }
             catch (Exception ex)
             {
@@ -74,26 +74,44 @@ namespace NidhiWebsite.Controllers
                                        item_group_name = m.item_group_name,
                                        item_group_code = m.item_group_code,
                                    });
-                if(allitemgroup.Any(m => m.item_group_name == data.item_group_name && (m.item_group_id !=data.item_group_id)))
+                if (allitemgroup.Any(m => m.item_group_name == data.item_group_name && (m.item_group_id != data.item_group_id)))
                 {
                     return "Item group name already exist";
                 }
-                if(allitemgroup.Any(m => (m.item_group_code == data.item_group_code) && (m.item_group_id != data.item_group_id)))
+                if (allitemgroup.Any(m => (m.item_group_code == data.item_group_code) && (m.item_group_id != data.item_group_id)))
                 {
                     return "Item group code already exist";
                 }
 
                 var productdata = new ItemGroupModel();
-                productdata = new ItemGroupModel
+                if (data.item_group_id == 0)
                 {
-                    item_group_name= data.item_group_name,
-                    item_group_code = data.item_group_code,
-                    item_group_image = data.item_group_image,
-                    item_group_description = data.item_group_description,
-                    item_group_user_id = data.item_group_user_id,
-                    item_group_row_date = DateTime.UtcNow,
-                };
-                datacontext.Data_tbl_Item_group.Add(productdata);
+                    productdata = new ItemGroupModel
+                    {
+                        item_group_name = data.item_group_name,
+                        item_group_code = data.item_group_code,
+                        item_group_image = data.item_group_image,
+                        item_group_description = data.item_group_description,
+                        item_group_user_id = data.item_group_user_id,
+                        item_group_row_date = DateTime.UtcNow,
+                    };
+
+                    datacontext.Data_tbl_Item_group.Add(productdata);
+                }
+                else
+                {
+                    productdata = datacontext.Data_tbl_Item_group.FirstOrDefault(m => m.item_group_id == data.item_group_id);
+                    if (productdata != null)
+                    {
+                        productdata.item_group_name = data.item_group_name;
+                        productdata.item_group_code = data.item_group_code;
+                        productdata.item_group_image = data.item_group_image;
+                        productdata.item_group_description = data.item_group_description;
+                        productdata.item_group_user_id = data.item_group_user_id;
+                        productdata.item_group_row_date = DateTime.UtcNow;
+                        datacontext.Data_tbl_Item_group.Update(productdata);
+                    }
+                }
                 datacontext.SaveChanges();
                 return "success";
             }
@@ -110,7 +128,7 @@ namespace NidhiWebsite.Controllers
             try
             {
                 var itemgroup = datacontext.Data_tbl_Item_group.AsNoTracking().
-                                Where(l =>l.item_group_id!=0).
+                                Where(l => l.item_group_id != 0).
                                 Select(m => new
                                 {
                                     item_group_id = m.item_group_id,
@@ -124,6 +142,49 @@ namespace NidhiWebsite.Controllers
             }
         }
         #endregion
+        [HttpGet("GetAllItemGroup")]
+        public IActionResult GetAllItemGroup()
+        {
+            try
+            {
+                var itemgroup = datacontext.Data_tbl_Item_group.AsNoTracking().
+                                Where(l => l.item_group_id != 0).
+                                Select(m => new
+                                {
+                                    item_group_id = m.item_group_id,
+                                    item_group_name = m.item_group_name,
+                                    item_group_code = m.item_group_code,
+                                    item_group_description = m.item_group_description,
+                                }).ToArray();
+                return Ok(itemgroup);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPost("GetItemGroup")]
+        public IActionResult GetItemGroup(int itemgroupid)
+        {
+            try
+            {
+                var itemgroup = datacontext.Data_tbl_Item_group.AsNoTracking().
+                                Where(l => l.item_group_id == itemgroupid).
+                                Select(m => new
+                                {
+                                    item_group_id = m.item_group_id,
+                                    item_group_name = m.item_group_name,
+                                    item_group_code = m.item_group_code,
+                                    item_group_description = m.item_group_description,
+                                    item_group_image = m.item_group_image,
+                                }).FirstOrDefault();
+                return Ok(itemgroup);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
 
     }
 }
